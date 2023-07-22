7bd5500a33bfa26e4ab1abe2e8225a89cfc751cd
sumitXcoder <sumitkumar230404@gmail.com>
2023-07-22 13:17:30 +0530

    heart icon update,navbar position->sticky

--- js/main.js
+++ js/main.js
@@ -2,7 +2,19 @@ function googleTranslateElementInit() {
     new google.translate.TranslateElement({ pageLanguage: 'en' }, 'google-translate');
 }
 for (const btn of document.getElementsByClassName("fa-heart"))
-    btn.onclick = e => { e.target.style.color = e.target.style.color !== "red" ? "red" : "black" }
+    btn.onclick = e => { 
+        if(e.target.style.color!=="red"){
+            e.target.classList.remove("fa-regular")
+            e.target.classList.add("fa-solid")
+            e.target.classList.add("fa-bounce")
+            e.target.style.color="red"
+        }else{
+            e.target.classList.remove("fa-solid")
+            e.target.classList.remove("fa-bounce")
+            e.target.classList.add("fa-regular")
+            e.target.style.color="black"
+        }
+    }
 const slide = document.querySelector(".slide")
 var slideShow = setInterval(() => slide.scrollLeft += WIDTH, 2000)
 setTimeout(() => clearInterval(slideShow), 8000)
